8fdabe6474c5ad7494f9dd4f0cf1e9694d751a5f
Ahmad Mufied Nugroho <40788381+Ahmad-mufied@users.noreply.github.com>
2022-01-31 00:07:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ const card = document.querySelector('.card__inner');
 card.addEventListener('click', function () {
   card.classList.toggle('is-flipped');
 });
+
+const buttons = document.querySelectorAll('a');
+buttons.forEach(btn => {
+  btn.addEventListener('click', function(e) {
+
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+      ripples.remove()
+    },1000);
+  })
+})
